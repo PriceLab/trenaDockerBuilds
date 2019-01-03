2e3b51b751c48ff4a7a70ac0678ae2c7fca93b7d
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-01-02 17:01:46 -0800

    all the volatile code, more or less...

---
+++ trenaLatest/installShinyLibraries.R
@@ -0,0 +1,22 @@
+printf <- function(...) print(noquote(sprintf(...)))
+biocGet <- function(pkgs){
+   library(BiocManager)
+   BiocManager::install(pkgs)
+   }
+
+code.pkgs <- c("shiny",
+               "shinyjs",
+               "shinydashboard")
+
+for(code.pkg in code.pkgs){
+   suppressWarnings(
+      needed <- !require(code.pkg, character.only=TRUE, quiet=TRUE)
+      )
+   printf("%s needed? %s", code.pkg, needed)
+   if(needed)
+      biocGet(code.pkg)
+   } # for
+
+
+
+
